a167bfe01d43c870ffc56ab7a9481aae0d27626c
James Essmiller <jamestoast@cox.net>
2016-03-05 20:00:49 -0600

    Didn't accomplish much, but figured out a whole lot of things not to do. Created temp materials to mark bottles. Created testing scene to play with bottles.

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+
+	public float speed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.y += speed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
