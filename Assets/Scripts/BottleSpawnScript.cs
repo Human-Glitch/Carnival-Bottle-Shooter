b98043f1c0999fd476728c348a98a8a8d60d4216
unknown <Jamestoast@cox.net>
2016-03-07 00:53:09 -0600

    BottleSpawnScript now generates targets and sends them moving in a direction! So far, it only works for one object. But it's pretty cool. Also put the FTB files from Andrew in the "FromBlender" file in resources.

--- Assets/Scripts/BottleSpawnScript.cs
+++ Assets/Scripts/BottleSpawnScript.cs
@@ -1,43 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Diagnostics;
 
 public class BottleSpawnScript : MonoBehaviour {
 
 	private GameObject[] targets;
-	private Stopwatch stopwatch;
 	[SerializeField] private float speed = 500f;
+	[SerializeField] private float xOffset = 0f;
+	[SerializeField] private float yOffset = 0f;
+	[SerializeField] private float zOffset = 0f;
+	public float spawnTime = 2f;
+	private float timeToSpawn = 0f;
 
 	// Use this for initialization
 	void Start () {
-		targets = new GameObject[5];
 
-		for (int x = 0; x < targets.Length; x++) {
+		targets = new GameObject[6];
+
+		for (int x = 1; x < targets.Length; x++) {
 			targets[x] = Resources.Load ("Target"+(x+1)) as GameObject;
 		}
 
-		targets [0] = Resources.Load ("Target1Test") as GameObject;
-		stopwatch = Stopwatch.StartNew();
+		targets [0] = Resources.Load ("Platform") as GameObject;
+
+		targets [1] = Resources.Load ("Target1Test") as GameObject;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (stopwatch.ElapsedMilliseconds > 20) {
+		timeToSpawn -= Time.deltaTime;
+
+		if (timeToSpawn <= 0) {
+			timeToSpawn = spawnTime;
+			//timeToSpawn = 9999999999999f;
 			Spawn ();
-			stopwatch.Reset ();
-			//stopwatch = Stopwatch.StartNew ();
 		}
 	}
 
 	public void Spawn() {
 		int num = (int)(Random.value * 5 + 1);
-		print(num);
-		GameObject target = Instantiate(targets[0], new Vector3(-5, 0, 0), Quaternion.identity) as GameObject;
-		//target.GetComponent<Rigidbody> ().velocity = new Vector3 (speed, 0f, 0f);
-		Rigidbody[] rb = target.GetComponentsInChildren<Rigidbody> ();
-		for (int x = 0; x < rb.Length; x++) {
-			rb [x].velocity = new Vector3 (speed, 0f, 0f);
+		if (num == 6)
+			num = 5;
+		
+		float objX = 0f;
+		float objY = 0f;
+		float objZ = 0f;
+
+		switch (num) {
+			case 1:
+				// Milk Bottles
+				objX = -0.91f;
+				objY = -1.52f;
+				objZ = -17.73f;
+				break;
+
+			case 2:
+				// Object 2
+				objX = 0f;
+				objY = 0f;
+				objZ = 0f;
+				break;
+
+			case 3:
+				// Object 3
+				objX = 0f;
+				objY = 0f;
+				objZ = 0f;
+				break;
+
+			case 4:
+				// Object 4
+				objX = 0f;
+				objY = 0f;
+				objZ = 0f;
+				break;
+
+			case 5:
+				// Object 5
+				objX = 0f;
+				objY = 0f;
+				objZ = 0f;
+				break;
+
+			default:
+				break;
 		}
+
+
+		//GameObject target = Instantiate(targets[num], new Vector3(objX + xOffset, objY + yOffset, objZ + zOffset), Quaternion.identity) as GameObject;
+		GameObject target = Instantiate(targets[1], new Vector3(-0.91f + xOffset, -1.52f + yOffset, -17.73f + zOffset), Quaternion.identity) as GameObject;
+		GameObject platform = Instantiate (targets [0], new Vector3 (xOffset, yOffset, zOffset), Quaternion.identity) as GameObject;
+		platform.GetComponent<PlatformScript> ().speed = speed;
+
+		Rigidbody[] children = target.GetComponentsInChildren<Rigidbody> ();
+
+		for (int x = 0; x < children.Length; x++) {
+			children [x].velocity = new Vector3 (speed, 0f, 0f);
+		}
+
+		//target.GetComponent<Rigidbody> ().velocity = new Vector3 (speed, 0f, 0f);
+		//target.GetComponent<PlatformScript>().speed = speed;
 		/*Rigidbody rigidbody = target.GetComponent<Rigidbody> ();
 		rigidbody.AddForce(new Vector3(speed, 0f, 0f));*/
 
--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public class PlatformScript : MonoBehaviour {
 
 	public float speed = 0f;
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		Vector3 pos = transform.position;
-		pos.y += speed * Time.deltaTime;
-		transform.position = pos;
+	// Update is called once per frame	
+	void FixedUpdate () {
+		rb.velocity = new Vector3 (speed, 0f, 0f);
 	}
 }
