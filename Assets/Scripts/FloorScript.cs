93069cd6ad434a931435931674bf9885e98bc385
Kody Buss <kody.buss@gmail.com>
2017-04-12 18:08:59 -0500

    Added lighting refinements and demo'd ideas
    More sophisticated lighting and atmosphere to compliment the game.

--- Assets/Scripts/FloorScript.cs
+++ Assets/Scripts/FloorScript.cs
@@ -16,7 +16,7 @@ public class FloorScript : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.name == "BottomTarget1" || other.name == "BottomTarget2" || other.name == "TopTarget" || other.name == "tennis_ball(Clone)") {
 			if(other.name != "tennis_ball(Clone)")
-				other.gameObject.GetComponent<AudioSource>().Play();
+				//other.gameObject.GetComponent<AudioSource>().Play();
 			Destroy (other.gameObject, 5);
 		}
 	}
---
+++ Assets/Scripts/MovingLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingLight : MonoBehaviour {
+
+	public float speed = 9f;
+
+	private float _rotationX = 9f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(0, _rotationX, 0);
+
+		_rotationX = Mathf.Clamp(_rotationX, -.5f, .5f);
+	
+	}
+}
